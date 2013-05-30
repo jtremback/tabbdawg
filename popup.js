90bb37a05ab896f3bacca843c9bb5605847e0f95
Jehan Tremback <jehan.tremback@gmail.com>
2013-05-29 23:31:47 -0700

    displays list of tabs open. Pretty useless, but hey

--- popup.js
+++ popup.js
@@ -1,4 +1,17 @@
-// Lily is cute ;)
 document.addEventListener('DOMContentLoaded', function () {
-  chrome.tabs.query({"currentWindow": true}, function(shibby){console.log(shibby)})
+  chrome.tabs.query({}, 
+    function(tabs){
+      var ul = document.getElementById("list");
+      for (var i = tabs.length - 1; i >= 0; i--) {
+        var tab = tabs[i],
+            newLi = document.createElement("li"),
+            newA = document.createElement("a");
+
+        newA.href = tab.url;
+        newA.textContent = tab.title;
+        newLi.appendChild(newA);
+        ul.appendChild(newLi);
+      }; 
+    }
+  )
 });
